ac5ec9ff8a2c95bb31b4af9d21f40156cf550c8b
Oleh <marko_polo@MacBookPro.lan>
2026-08-13 22:46:27 +0200

    Connect form to Google Apps Script Web App

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // НАСТРОЙКА: вставьте сюда URL вашего Google Apps Script Web App
 // (получите его, следуя инструкции в README.md, раздел 1)
 // ============================================================
-const GOOGLE_SCRIPT_URL = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_URL_HERE";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyMbPUeOtAJhGF8o2VPMFO4zu3Q3o3an6nFEL5X83NKw-xycMj8RJUcj5Z1W26_LxkAbQ/exec";
